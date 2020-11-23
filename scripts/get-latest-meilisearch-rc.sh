05516ac3109ebc9fba68f2cde38049696e87c7a1
Clémentine Urquizar <clementine@meilisearch.com>
2020-11-23 23:27:04 +0100

    Update get-latest-meilisearch-rc.sh

--- scripts/get-latest-meilisearch-rc.sh
+++ scripts/get-latest-meilisearch-rc.sh
@@ -8,5 +8,5 @@ curl -s 'https://api.github.com/repos/meilisearch/MeiliSearch/releases' > "$temp
 latest_rc_release=$(cat "$temp_file" \
     | grep -E 'tag_name' | grep 'rc' | head -1 \
     | tr -d ',"' | cut -d ':' -f2 | tr -d ' ')
-rm -rm "$temp_file"
+rm -rf "$temp_file"
 echo "$latest_rc_release"
